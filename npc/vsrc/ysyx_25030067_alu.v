48b6a57c64ddd3556e8efe891e2e7e0594ca59e6
crazyofcode <1710338943@qq.com>
2026-03-06 13:52:00 +0800

    Multiplexed adder

--- npc/vsrc/ysyx_25030067_alu.v
+++ npc/vsrc/ysyx_25030067_alu.v
@@ -5,8 +5,11 @@ module ysyx_25030067_alu (
   output [31:0] alu_result_o
 );
 
-  wire [31:0] add_res;
-  wire [31:0] sub_res;
+  // Multiplexed adder: a + b (add) or a + ~b + 1 (sub)
+  wire        is_sub = alu_op_i[0];
+  wire [31:0] adder_b = is_sub ? ~alu_b_i : alu_b_i;
+  wire [31:0] adder_res = alu_a_i + adder_b + {31'b0, is_sub};
+
   wire [31:0] and_res;
   wire [31:0] or_res;
   wire [31:0] xor_res;
@@ -14,8 +17,6 @@ module ysyx_25030067_alu (
   wire [31:0] srl_res;
   wire [31:0] sra_res;
 
-  assign add_res = alu_a_i + alu_b_i;
-  assign sub_res = alu_a_i - alu_b_i;
   assign and_res = alu_a_i & alu_b_i;
   assign or_res = alu_a_i | alu_b_i;
   assign xor_res = alu_a_i ^ alu_b_i;
@@ -23,8 +24,18 @@ module ysyx_25030067_alu (
   assign srl_res = alu_a_i >> alu_b_i[4:0];
   assign sra_res = $signed(alu_a_i) >>> alu_b_i[4:0];
 
-  assign alu_result_o = alu_op_i[2] ? (alu_op_i[1] ?  (alu_op_i[0] ? xor_res : or_res)  :
-                                                      (alu_op_i[0] ? and_res : srl_res)) :
-                                      (alu_op_i[1] ?  (alu_op_i[0] ? sra_res : sll_res) :
-                                                      (alu_op_i[0] ? sub_res : add_res));
+  wire [31:0] alu_hi_hi_res;
+  wire [31:0] alu_hi_lo_res;
+  wire [31:0] alu_lo_hi_res;
+  wire [31:0] alu_lo_lo_res;
+  wire [31:0] alu_hi_res;
+  wire [31:0] alu_lo_res;
+
+  assign alu_hi_hi_res = alu_op_i[0] ? xor_res : or_res;
+  assign alu_hi_lo_res = alu_op_i[0] ? and_res : srl_res;
+  assign alu_lo_hi_res = alu_op_i[0] ? sra_res : sll_res;
+  assign alu_lo_lo_res = adder_res;
+  assign alu_hi_res = alu_op_i[1] ? alu_hi_hi_res : alu_hi_lo_res;
+  assign alu_lo_res = alu_op_i[1] ? alu_lo_hi_res : alu_lo_lo_res;
+  assign alu_result_o = alu_op_i[2] ? alu_hi_res : alu_lo_res;
 endmodule
